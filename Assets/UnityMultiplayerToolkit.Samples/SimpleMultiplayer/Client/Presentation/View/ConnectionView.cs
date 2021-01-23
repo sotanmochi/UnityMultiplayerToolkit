b1110b49de183317b870d017343d148b63006268
sotanmochi <stmczk@gmail.com>
2021-01-23 19:27:20 +0900

    Add simple multiplayer sample

---
+++ Assets/UnityMultiplayerToolkit.Samples/SimpleMultiplayer/Client/Presentation/View/ConnectionView.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.UI;
+using UniRx;
+
+namespace UnityMultiplayerToolkit.Samples.SimpleMultiplayer
+{
+    public class ConnectionView : MonoBehaviour
+    {
+        [SerializeField] Toggle _Toggle_IsHost;
+        [SerializeField] Button _Button_StartClient;
+        [SerializeField] Button _Button_StopClient;
+
+        public IObservable<bool> IsHostAsObservable() => _Toggle_IsHost.OnValueChangedAsObservable();
+        public IObservable<Unit> OnClickStartClientAsObservable() => _Button_StartClient.OnClickAsObservable();
+        public IObservable<Unit> OnClickStopClientAsObservable() => _Button_StopClient.OnClickAsObservable();
+    }
+}
