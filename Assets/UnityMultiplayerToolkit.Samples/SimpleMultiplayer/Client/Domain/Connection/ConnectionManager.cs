5f92e225ecccef4b6fdb598b6a79e276868a1728
sotanmochi <stmczk@gmail.com>
2021-01-23 19:27:20 +0900

    Add simple multiplayer sample

---
+++ Assets/UnityMultiplayerToolkit.Samples/SimpleMultiplayer/Client/Domain/Connection/ConnectionManager.cs
@@ -0,0 +1,79 @@
+﻿using UnityEngine;
+using UniRx;
+using UnityMultiplayerToolkit.MLAPIExtension;
+
+namespace UnityMultiplayerToolkit.Samples.SimpleMultiplayer
+{
+    public class ConnectionManager : MonoBehaviour
+    {
+        [SerializeField] NetworkConfig _NetworkConfig;
+        [SerializeField] MLAPIClient _Client;
+        public bool IsHost;
+
+        void Awake()
+        {
+            _Client.OnClientConnectedAsObservable()
+            .Subscribe(clientId => 
+            {
+                Debug.Log("[SimpleMultiplayer] Connected client id: " + clientId);
+            })
+            .AddTo(this);
+
+            _Client.OnClientDisconnectedAsObservable()
+            .Subscribe(clientId => 
+            {
+                Debug.Log("[SimpleMultiplayer] Disconnected client id: " + clientId);
+            })
+            .AddTo(this);
+        }
+
+        void Start()
+        {
+            _Client.Initialize(_NetworkConfig);
+        }
+
+        void OnDestroy()
+        {
+            if (IsHost)
+            {
+                _Client.StopHost();
+            }
+            else
+            {
+                _Client.Disconnect();
+            }
+        }
+
+        public async void StartClient()
+        {
+            if (IsHost)
+            {
+                bool success = await _Client.StartHost();
+                if (success)
+                {
+                    Debug.Log("[SimpleMultiplayer] This client has started as a host.");
+                }
+            }
+            else
+            {
+                bool success = await _Client.Connect();
+                if (success)
+                {
+                    Debug.Log("[SimpleMultiplayer] This client has connected to the server.");
+                }
+            }
+        }
+
+        public void StopClient()
+        {
+            if (IsHost)
+            {
+                _Client.StopHost();
+            }
+            else
+            {
+                _Client.Disconnect();
+            }
+        }
+    }
+}
---
+++ Assets/UnityMultiplayerToolkit.Samples/SimpleMultiplayer/Client/Presentation/Presenter/ConnectionPresenter.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using UniRx;
+
+namespace UnityMultiplayerToolkit.Samples.SimpleMultiplayer
+{
+    public class ConnectionPresenter : MonoBehaviour
+    {
+        [SerializeField] ConnectionView _ConnectionView;
+        [SerializeField] ConnectionManager _ConnectionManager;
+
+        async void Awake()
+        {
+            _ConnectionView.IsHostAsObservable()
+            .Subscribe(isHost => 
+            {
+                _ConnectionManager.IsHost = isHost;
+            })
+            .AddTo(this);
+
+            _ConnectionView.OnClickStartClientAsObservable()
+            .Subscribe(_ => 
+            {
+                _ConnectionManager.StartClient();
+            })
+            .AddTo(this);
+
+            _ConnectionView.OnClickStopClientAsObservable()
+            .Subscribe(_ => 
+            {
+                _ConnectionManager.StopClient();
+            })
+            .AddTo(this);
+        }
+    }
+}
