cd2fce4f37fc027f4741d162c0035e4149287ef5
sotanmochi <stmczk@gmail.com>
2021-01-23 19:22:42 +0900

    Add MLAPI Extension

---
+++ Assets/UnityMultiplayerToolkit/MLAPIExtension/ConnectionConfig.cs
@@ -0,0 +1,49 @@
+﻿using System.IO;
+using UnityEngine;
+
+namespace UnityMultiplayerToolkit.MLAPIExtension
+{
+    [System.Serializable]
+    public class ConnectionConfig
+    {
+        public string Address;
+        public int Port;
+        public string Key;
+
+        public static ConnectionConfig GetDefault()
+        {
+            ConnectionConfig config = new ConnectionConfig();
+            config.Address = "127.0.0.1";
+            config.Port = 7777;
+            config.Key = "MultiplayerRoom";
+            return config;
+        }
+
+        public static ConnectionConfig LoadConfigFile()
+        {
+            if (!File.Exists(ConfigFilePath))
+            {
+                var defaultConfig = GetDefault();
+                defaultConfig.SaveConfigFile();
+                return defaultConfig;
+            }
+            string jsonStr = File.ReadAllText(ConfigFilePath);
+            var config = JsonUtility.FromJson<ConnectionConfig>(jsonStr);
+            return config;
+        }
+
+        public void SaveConfigFile()
+        {
+            string jsonStr = JsonUtility.ToJson(this);
+            File.WriteAllText(ConfigFilePath, jsonStr);
+        }
+
+        protected static string ConfigFilePath
+        {
+            get
+            {
+                return Path.Combine(Application.dataPath, "MultiplayerConnectionConfig.json");
+            }
+        }
+    }
+}
---
+++ Assets/UnityMultiplayerToolkit/MLAPIExtension/NetworkConfig.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+namespace UnityMultiplayerToolkit.MLAPIExtension
+{
+    [System.Serializable]
+    [CreateAssetMenu(menuName = "Unity Multiplayer Toolkit/MLAPI/Create Network Config", fileName = "MLAPINetworkConfig")]
+    public class NetworkConfig : ScriptableObject
+    {
+        public bool EnableSceneManagement = false;
+        public bool ConnectionApproval = false;
+        public bool CreatePlayerPrefab = false;
+        public bool ForceSamePrefabs = false;
+        public bool RecycleNetworkIds = false;
+    }
+}
