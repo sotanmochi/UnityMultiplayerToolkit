7a8d010747fd6b2b7bd8d7f02b174c4b64ea0969
sotanmochi <stmczk@gmail.com>
2021-01-27 21:44:44 +0900

    Update simple multiplayer sample

--- Assets/UnityMultiplayerToolkit.Samples/SimpleMultiplayer/Client/Domain/Messaging/MessagingHub.cs
+++ Assets/UnityMultiplayerToolkit.Samples/SimpleMultiplayer/Client/Domain/Messaging/MessagingHub.cs
@@ -1,4 +1,8 @@
-﻿using UnityEngine;
+﻿using System;
+using System.IO;
+using UnityEngine;
+using UniRx;
+using MLAPI.Messaging;
 using MLAPI.Serialization.Pooled;
 using UnityMultiplayerToolkit.MLAPIExtension;
 
@@ -8,6 +12,9 @@ namespace UnityMultiplayerToolkit.Samples.SimpleMultiplayer.Client
     public class MessagingHub : MonoBehaviour
     {
         [SerializeField] MLAPIClient _MLAPIClient;
+
+        public IObservable<(ulong senderId, string message)> OnReceivedTextMessageAsObservable() => _OnReceivedTextMessageSubject;
+        private Subject<(ulong senderId, string message)> _OnReceivedTextMessageSubject = new Subject<(ulong senderId, string message)>();
 
         void Awake()
         {
@@ -21,12 +28,14 @@ namespace UnityMultiplayerToolkit.Samples.SimpleMultiplayer.Client
 
         void RegisterNamedMessageHandlers()
         {
-
+            CustomMessagingManager.RegisterNamedMessageHandler(MessagingHubConstants.SEND_TEXT_MESSAGE_TO_ALL_CLIENTS, MessageHandler_SendTextMessageToAllClients);
+            CustomMessagingManager.RegisterNamedMessageHandler(MessagingHubConstants.SEND_TEXT_MESSAGE_TO_CLIENTS_EXCEPT_SELF, MessageHandler_SendTextMessageExceptSelf);
         }
 
         void UnregisterNamedMessageHandlers()
         {
-
+            CustomMessagingManager.UnregisterNamedMessageHandler(MessagingHubConstants.SEND_TEXT_MESSAGE_TO_ALL_CLIENTS);
+            CustomMessagingManager.UnregisterNamedMessageHandler(MessagingHubConstants.SEND_TEXT_MESSAGE_TO_CLIENTS_EXCEPT_SELF);
         }
 
 #region Client to Server
@@ -43,10 +52,53 @@ namespace UnityMultiplayerToolkit.Samples.SimpleMultiplayer.Client
             }
         }
 
+        public void SendTextMessageToAllClients(string message)
+        {
+            using (PooledBitStream stream = PooledBitStream.Get())
+            {
+                using (PooledBitWriter writer = PooledBitWriter.Get(stream))
+                {
+                    writer.WriteStringPacked(message);
+                    _MLAPIClient.SendMessageToServer(MessagingHubConstants.SEND_TEXT_MESSAGE_TO_ALL_CLIENTS, stream);
+                }
+            }
+        }
+
+        public void SendTextMessageToClientsExceptSelf(string message)
+        {
+            using (PooledBitStream stream = PooledBitStream.Get())
+            {
+                using (PooledBitWriter writer = PooledBitWriter.Get(stream))
+                {
+                    writer.WriteStringPacked(message);
+                    _MLAPIClient.SendMessageToServer(MessagingHubConstants.SEND_TEXT_MESSAGE_TO_CLIENTS_EXCEPT_SELF, stream);
+                }
+            }
+        }
+
 #endregion
 
-#region Server to Client
+#region Receivers
 
+        private void MessageHandler_SendTextMessageToAllClients(ulong sender, Stream dataStream)
+        {
+            using (PooledBitReader reader = PooledBitReader.Get(dataStream))
+            {
+                ulong senderClientId = reader.ReadUInt64Packed();
+                string message = reader.ReadStringPacked().ToString();
+                _OnReceivedTextMessageSubject.OnNext((senderClientId, message));
+            }
+        }
+
+        private void MessageHandler_SendTextMessageExceptSelf(ulong sender, Stream dataStream)
+        {
+            using (PooledBitReader reader = PooledBitReader.Get(dataStream))
+            {
+                ulong senderClientId = reader.ReadUInt64Packed();
+                string message = reader.ReadStringPacked().ToString();
+                _OnReceivedTextMessageSubject.OnNext((senderClientId, message));
+            }
+        }
 
 #endregion
 
--- Assets/UnityMultiplayerToolkit.Samples/SimpleMultiplayer/Client/Presentation/View/TextMessageView.cs
+++ Assets/UnityMultiplayerToolkit.Samples/SimpleMultiplayer/Client/Presentation/View/TextMessageView.cs
@@ -9,15 +9,24 @@ namespace UnityMultiplayerToolkit.Samples.SimpleMultiplayer.Client
     {
         [SerializeField] InputField _Text_Message;
         [SerializeField] Button _Button_SendToServer;
+        [SerializeField] Button _Button_SendToAllClients;
 
         public IObservable<string> SendToServerAsObservable() => _SendToServerSubject;
         private Subject<string> _SendToServerSubject = new Subject<string>();
+        public IObservable<string> SendToAllClientsAsObservable() => _SendToAllClientsSubject;
+        private Subject<string> _SendToAllClientsSubject = new Subject<string>();
 
         void Awake()
         {
             _Button_SendToServer.OnClickAsObservable().Subscribe(_ =>
             {
                 _SendToServerSubject.OnNext(_Text_Message.text);
+            })
+            .AddTo(this);
+
+            _Button_SendToAllClients.OnClickAsObservable().Subscribe(_ =>
+            {
+                _SendToAllClientsSubject.OnNext(_Text_Message.text);
             })
             .AddTo(this);
         }
