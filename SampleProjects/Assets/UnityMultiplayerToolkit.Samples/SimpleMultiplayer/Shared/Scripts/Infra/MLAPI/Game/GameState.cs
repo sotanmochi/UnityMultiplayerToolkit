ce3e37751a7766c4d28ed3486d4f7a34076e81e8
sotanmochi <stmczk@gmail.com>
2021-04-20 08:39:26 +0900

    Update sample project

---
+++ SampleProjects/Assets/UnityMultiplayerToolkit.Samples/SimpleMultiplayer/Shared/Scripts/Infra/MLAPI/Game/GameState.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+using UniRx;
+using MLAPI;
+using MLAPI.NetworkVariable;
+using MLAPI.NetworkVariable.Collections;
+
+namespace UnityMultiplayerToolkit.Samples.SimpleMultiplayer.Shared
+{
+    [RequireComponent(typeof(NetworkObject))]
+    public class GameState : NetworkBehaviour
+    {
+
+    }
+}
