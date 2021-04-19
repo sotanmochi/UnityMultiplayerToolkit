68d350fe8ad9c1fb81bea1c2fa76a3446fd396b4
sotanmochi <stmczk@gmail.com>
2021-04-20 08:53:18 +0900

    Update sample project (WIP)

---
+++ SampleProjects/Assets/UnityMultiplayerToolkit.Samples/SimpleMultiplayer/Shared/Scripts/Application/ApplicationInitializer.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using UnityMultiplayerToolkit.Samples.Utility;
+
+namespace UnityMultiplayerToolkit.Samples.SimpleMultiplayer
+{
+    public class ApplicationInitializer : MonoBehaviour, IInitializableBeforeSceneLoad
+    {
+        public void InitializeBeforeSceneLoad()
+        {
+            UnityEngine.QualitySettings.vSyncCount = 0;
+            UnityEngine.Application.targetFrameRate = 60;
+            UnityEngine.Application.runInBackground = true;
+        }
+    }
+}
